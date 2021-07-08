25e2ae58789548339f707042becdb61bce14a7d6
Anbarasan <anbarasanj24@gmail.com>
2021-07-08 17:24:28 +0530

    Congif changes

--- products/src/index.js
+++ products/src/index.js
@@ -2,11 +2,9 @@ import faker from 'faker';
 
 let products = '';
 
-for (let index = 0; index < 3; index++) {
-    const name = faker.commerce.productName();
-    products += `<h1>${name}</h1>`
-
+for (let i = 0; i < 5; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
 }
-document.querySelector('#products').innerHTML = products
 
-
+document.querySelector('#dev-products').innerHTML = products;
